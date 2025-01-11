01d11ce05c5c9019c647c6463d6e89664209cd52
YU <111847207+YU000jp@users.noreply.github.com>
2025-01-12 08:41:04 +0900

    feat(): translations

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import '@logseq/libs' //https://plugins-doc.logseq.com/
-import { settingsTemplate } from './settings'
-import { t } from 'logseq-l10n'
 import { LSPluginBaseInfo } from '@logseq/libs/dist/LSPlugin.user'
+import { setup as l10nSetup, t } from 'logseq-l10n'
+import { settingsTemplate } from './settings'
+import af from "./translations/af.json"
+import de from "./translations/de.json"
+import es from "./translations/es.json"
+import fr from "./translations/fr.json"
+import id from "./translations/id.json"
+import it from "./translations/it.json"
+import ja from "./translations/ja.json"
+import ko from "./translations/ko.json"
+import nbNO from "./translations/nb-NO.json"
+import nl from "./translations/nl.json"
+import pl from "./translations/pl.json"
+import ptBR from "./translations/pt-BR.json"
+import ptPT from "./translations/pt-PT.json"
+import ru from "./translations/ru.json"
+import sk from "./translations/sk.json"
+import tr from "./translations/tr.json"
+import uk from "./translations/uk.json"
+import zhCN from "./translations/zh-CN.json"
+import zhHant from "./translations/zh-Hant.json"
 
 const keyToolbar = "cssSnippet"
 const keyCss = "cssSnippet"
 
-// L10n
-// import { setup as l10nSetup, t } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
-// import ja from "./translations/ja.json"
 
 const createSnippetItem = () => {
   const snippetItem = document.createElement('details')
@@ -71,6 +87,7 @@ const createSnippetItem = () => {
 
   const textarea = document.createElement('textarea')
   textarea.className = 'css-editor form-input language-css'
+  textarea.placeholder = t('Enter CSS here') // ここにCSSを入力してください
   snippetItem.appendChild(textarea)
 
   snippetItem.addEventListener('dragstart', (e) => {
@@ -251,8 +268,12 @@ const addEventListenerOnce = (element: HTMLElement, event: string, handler: Even
 /* main */
 const main = async () => {
 
-  // L10n
-  // await l10nSetup({ builtinTranslations: { ja } })
+  // l10nのセットアップ
+  await l10nSetup({
+    builtinTranslations: {//Full translations
+      ja, af, de, es, fr, id, it, ko, "nb-NO": nbNO, nl, pl, "pt-BR": ptBR, "pt-PT": ptPT, ru, sk, tr, uk, "zh-CN": zhCN, "zh-Hant": zhHant
+    }
+  })
 
   /* user settings */
   logseq.useSettingsSchema(settingsTemplate())
